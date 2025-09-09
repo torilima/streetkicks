96cf015f66aacc69c98daa8c32e68c99fa1e559c
torilima <abelyvia.tori@ui.ac.id>
2025-09-10 00:57:55 +0700

    streetkicks

--- main/models.py
+++ main/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 from django.db import models
 class Product(models.Model):
+    CATEGORY_CHOICES = (
+        ('shoes', 'Shoes'),
+        ('jersey', 'Jersey'),
+        ('accessories', 'Accessories'),
+        ('ball', 'Ball'),
+    )
+
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    thumbnail = models.URLField()
-    category = models.CharField(max_length=100)
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
