82c9ce15a6f0694f089086517a5fc42ef85aa8c4
torilima <abelyvia.tori@ui.ac.id>
2025-09-10 17:03:10 +0700

    streetkick final

--- main/models.py
+++ main/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default='update')
+    category = models.CharField(max_length=250, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
