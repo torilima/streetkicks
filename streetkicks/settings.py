5d3ccf50a6f479b4ceaac74664e342494b7a958b
torilima <abelyvia.tori@ui.ac.id>
2025-09-21 21:55:43 +0700

    pushhhh

--- streetkicks/settings.py
+++ streetkicks/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "abelyvia-tori-streetkicks.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://abelyvia-tori-streetkicks.pbp.cs.ui.ac.id/"
+    "abelyvia-tori-streetkicks.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
